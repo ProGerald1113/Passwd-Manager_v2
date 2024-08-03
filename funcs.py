714fa979747936666979c3b23da32f9b2613584e
ProGerald1113 <brown.gerald.2008@gmail.com>
2024-08-03 08:18:52 +0100

    functions

--- funcs.py
+++ funcs.py
@@ -1,15 +1,22 @@
 import os
 
 def username_dir(username):
-    
     path = username
     os.mkdir(path)
 
-def write_passwd(passwd,parent_dir):
-    parent_dir = parent_dir
-    filename = "hashed_passwd.txt"
-    file_path = os.path.join(parent_dir,filename)
-    with open(file_path, "wb") as file:
-        file.write(passwd)
+def write_passwd(passwd,username):
 
+    path = f"/Passwd-Manager_v2/{username}"
+    print(path)
+
+    if os.path.exists(path):
+        with open("hashed_passwd.txt","ab+") as file:
+            file.write(passwd.encode())
+    
+    else:
+        pass
+
+username = "gee12"
+username_dir(username)
+write_passwd("njafkjs",username)
 
