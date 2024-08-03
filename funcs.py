2376e9042de35eeaacda4d227fadd331cfeebb97
ProGerald1113 <brown.gerald.2008@gmail.com>
2024-08-03 02:06:39 +0100

    Functions

--- funcs.py
+++ funcs.py
@@ -5,4 +5,11 @@ def username_dir(username):
     path = username
     os.mkdir(path)
 
-username_dir("Gerald")
+def write_passwd(passwd,parent_dir):
+    parent_dir = parent_dir
+    filename = "hashed_passwd.txt"
+    file_path = os.path.join(parent_dir,filename)
+    with open(file_path, "wb") as file:
+        file.write(passwd)
+
+
